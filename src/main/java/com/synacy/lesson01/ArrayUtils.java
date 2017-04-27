3bdfe84d8f83bc49be2464c9af375b426b15bf63
edwardGolez <pauledward@synacy.com>
2017-04-27 23:35:39 +0800

    First lesson introduction to JUnit

---
+++ src/main/java/com/synacy/lesson01/ArrayUtils.java
@@ -0,0 +1,40 @@
+package com.synacy.lesson01;
+
+public class ArrayUtils {
+
+	public static boolean contains(int[] array, int target) {
+
+		for (int element : array) {
+			if (element == target) {
+				return true;
+			}
+		}
+
+		return false;
+	}
+
+	public static int elementAt(int[] array, int index) {
+		if (index < 0 || index >= array.length) {
+			throw new IllegalArgumentException("Invalid index for array of lenght " + array.length + ": " + index);
+		}
+
+		return array[index];
+	}
+
+	public static void sort(int[] array) {
+		int arrayLength = array.length;
+		int temp = 0;
+
+		for (int i = 0; i < arrayLength; i++) {
+			for (int j = 1; j < (arrayLength - i); j++) {
+				if (array[j - 1] > array[j]) {
+					temp = array[j - 1];
+					array[j - 1] = array[j];
+					array[j] = temp;
+				}
+
+			}
+		}
+	}
+
+}
