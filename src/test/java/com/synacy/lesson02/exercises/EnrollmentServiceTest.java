c9ce610f6373c5bb0394976500092a3518d565f0
JR Comision <catalino.comision@outlook.com>
2017-05-03 16:57:14 +0800

    Add lesson02 to test directory and create test class for EnrollmentService

---
+++ src/test/java/com/synacy/lesson02/exercises/EnrollmentServiceTest.java
@@ -0,0 +1,17 @@
+package com.synacy.lesson02.exercises;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+@RunWith(MockitoJUnitRunner.class)
+public class EnrollmentServiceTest {
+
+    private EnrollmentService enrollmentService;
+
+    @Test
+    public void processEnrollment() throws Exception {
+
+    }
+
+}
