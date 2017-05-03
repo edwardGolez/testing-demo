23035a4bef39cea473b2b42b34af6a9864a9d50f
Kenichi Go Uang <kenichi.uang@synacy.com>
2017-05-03 17:50:53 +0800

    Make successful add test
    Signed-off-by: Kenichi Go Uang <kenichi.uang@synacy.com>

--- src/test/java/com/synacy/lesson01/exercises/ComplexNumberTest.java
+++ src/test/java/com/synacy/lesson01/exercises/ComplexNumberTest.java
@@ -2,29 +2,36 @@ package com.synacy.lesson01.exercises;
 
 import org.junit.Assert;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.junit.MockitoJUnitRunner;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.fail;
 
 /**
  * Created by kenichigouang on 5/3/17.
  */
+
+@RunWith(MockitoJUnitRunner.class)
 public class ComplexNumberTest {
     @Test
-    public void add_ShouldReturn5Real4ImaginaryComplexNumber() throws Exception {
+    public void add_shouldReturn5Real4ImaginaryComplexNumber() throws Exception {
         ComplexNumber complexNumber = new ComplexNumber(2, 4);
-        ComplexNumber anotherComplexNumber = new ComplexNumber(3, 0);
+        ComplexNumber anotherComplexNumber = new ComplexNumber(3, -2);
 
         ComplexNumber result = complexNumber.add(anotherComplexNumber);
 
-        Assert.assertEquals(new ComplexNumber(5,4), result);
+        Assert.assertEquals(5, result.getReal(), .001);
+        Assert.assertEquals(2, result.getImaginary(), .001);
     }
 
     @Test
     public void subtract() throws Exception {
+        Assert.fail();
     }
 
     @Test
     public void multiply() throws Exception {
+        Assert.fail();
     }
 
 }
