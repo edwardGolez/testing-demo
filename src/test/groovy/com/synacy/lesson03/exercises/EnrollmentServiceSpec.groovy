75d0b603744d90f48f3753648448699b652b6e99
Kenichi Go Uang <kenichi.uang@synacy.com>
2017-05-04 14:28:22 +0800

    Create spec to test if processEnrollment updates student profile
    Signed-off-by: Kenichi Go Uang <kenichi.uang@synacy.com>

---
+++ src/test/groovy/com/synacy/lesson03/exercises/EnrollmentServiceSpec.groovy
@@ -0,0 +1,53 @@
+package com.synacy.lesson03.exercises
+
+import com.synacy.lesson02.exercises.CourseClassService
+import com.synacy.lesson02.exercises.EnrollmentNotificationService
+import com.synacy.lesson02.exercises.EnrollmentService
+import com.synacy.lesson02.exercises.StudentProfileService
+import com.synacy.lesson02.exercises.StudyLoadFormatter
+import com.synacy.lesson02.exercises.SystemService
+import com.synacy.lesson02.exercises.domain.EnrollmentDto
+import com.synacy.lesson02.exercises.domain.StudentProfile
+import org.mockito.Mock
+
+/**
+ * Created by kenichigouang on 5/4/17.
+ */
+class EnrollmentServiceSpec extends spock.lang.Specification {
+
+    EnrollmentService service
+
+    StudentProfileService studentProfileService
+    CourseClassService courseClassService
+    EnrollmentNotificationService enrollmentNotificationService
+    StudyLoadFormatter studyLoadFormatter
+    SystemService systemService
+
+    void setup() {
+        service = new EnrollmentService()
+
+        studentProfileService = Mock()
+        courseClassService = Mock()
+        enrollmentNotificationService = Mock()
+        studyLoadFormatter = Mock()
+        systemService = Mock()
+
+        service.studentProfileService = studentProfileService
+        service.courseClassService = courseClassService
+        service.enrollmentNotificationService = enrollmentNotificationService
+        service.studyLoadFormatter = studyLoadFormatter
+        service.systemService = systemService
+    }
+
+    def "ProcessEnrollment should update student profile on processing of enrollment"() {
+        given:
+        EnrollmentDto enrollmentDetails = Mock()
+        StudentProfile studentProfile = Mock()
+
+        when:
+        service.processEnrollment(enrollmentDetails)
+
+        then:
+        1 * studentProfileService.updateStudentProfile(studentProfile)
+    }
+}
